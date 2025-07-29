816dc07896056ed20560f8e2c8496490e851c5ea
SanjalJain <sanjaljain2019@gmail.com>
2025-07-29 21:22:44 +0530

    third fix

--- api/bfhl.js
+++ api/bfhl.js
@@ -1,3 +1,43 @@
+// export default function handler(req, res) {
+//   if (req.method === 'POST') {
+//     const input = req.body.data || [];
+
+//     let even = [], odd = [], alpha = [], specialChars = [];
+//     let sum = 0;
+
+//     input.forEach(item => {
+//       if (!isNaN(item)) {
+//         const num = Number(item);
+//         num % 2 === 0 ? even.push(item) : odd.push(item);
+//         sum += num;
+//       } else if (/^[a-zA-Z]+$/.test(item)) {
+//         alpha.push(item.toUpperCase());
+//       } else {
+//         specialChars.push(item);
+//       }
+//     });
+
+//     let concatStr = alpha.join('').split('').reverse().map((char, i) =>
+//       i % 2 === 0 ? char.toUpperCase() : char.toLowerCase()
+//     ).join('');
+
+//     res.status(200).json({
+//       is_success: true,
+//       user_id: "sanjal_jain_17092004",
+//       email: "sanjal2248.be22@chitkara.edu.in",
+//       roll_number: "2210992248",
+//       odd_numbers: odd,
+//       even_numbers: even,
+//       alphabets: alpha,
+//       special_characters: specialChars,
+//       sum: sum.toString(),
+//       concat_string: concatStr
+//     });
+//   } else {
+//     res.status(405).json({ message: 'Method Not Allowed' });
+//   }
+// }
+
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const input = req.body.data || [];
@@ -8,7 +48,7 @@ export default function handler(req, res) {
     input.forEach(item => {
       if (!isNaN(item)) {
         const num = Number(item);
-        num % 2 === 0 ? even.push(item) : odd.push(item);
+        (num % 2 === 0 ? even : odd).push(item);
         sum += num;
       } else if (/^[a-zA-Z]+$/.test(item)) {
         alpha.push(item.toUpperCase());
@@ -17,9 +57,9 @@ export default function handler(req, res) {
       }
     });
 
-    let concatStr = alpha.join('').split('').reverse().map((char, i) =>
-      i % 2 === 0 ? char.toUpperCase() : char.toLowerCase()
-    ).join('');
+    const concatStr = alpha.join('').split('').reverse()
+      .map((char, i) => i % 2 === 0 ? char.toUpperCase() : char.toLowerCase())
+      .join('');
 
     res.status(200).json({
       is_success: true,
@@ -34,6 +74,6 @@ export default function handler(req, res) {
       concat_string: concatStr
     });
   } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
+    res.status(405).json({ error: 'Only POST method is allowed' });
   }
 }
